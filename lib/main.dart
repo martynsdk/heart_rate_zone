88b18a7176b73e913cef49b3e02160360ff0af1e
martynsdk <martynsdk@gmail.com>
2022-07-02 15:32:07 -0300

    Checkpoint for lunch

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,8 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+  static final String title = 'Heart Rate Zone';
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     LocalJsonLocalization.delegate.directories = ['lib/i18n'];
@@ -45,18 +45,9 @@ class MyApp extends StatelessWidget {
       title: "Heart Rate Zone",
       theme: ThemeData(
         useMaterial3: true,
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: "title".i18n()),
+      home: MyHomePage(title: title),
     );
   }
 }
@@ -77,26 +68,16 @@ class _MyHomePageState extends State<MyHomePage> {
 
   final selectedColor = Colors.white;
   final unselectedColor = Colors.white60;
-  final labelStyle = TextStyle(fontWeight: FontWeight.bold, fontSize: 16);
+  final labelStyle = const TextStyle(fontWeight: FontWeight.bold, fontSize: 16);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
         centerTitle: true,
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -108,18 +89,40 @@ class _MyHomePageState extends State<MyHomePage> {
                   setState(() => this.index = index),
               leading: Material(
                 clipBehavior: Clip.hardEdge,
-                shape: CircleBorder(),
+                shape: const CircleBorder(),
                 child: Ink.image(
                   width: 62,
                   height: 62,
                   fit: BoxFit.fitHeight,
-                  image: NetworkImage(
+                  image: const NetworkImage(
                     'https://images.unsplash.com/photo-1485893086445-ed75865251e0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
                   ),
                   child: InkWell(
                     onTap: () => setState(() => isExtended = !isExtended),
                   ),
                 ),
+              ),
+              trailing: AnimatedRailWidget(
+                child: isExtended
+                    ? Row(
+                  children: const [
+                    Icon(
+                      Icons.logout,
+                      color: Colors.white,
+                      size: 28,
+                    ),
+                    SizedBox(width: 12),
+                    Text(
+                      'Logout',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                )
+                    : const Icon(Icons.logout, color: Colors.white),
               ),
 
 
